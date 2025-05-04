b8d652454e266cde1d3cc2980f46d8bbb7afd210
vijayakrishna92 <vijaykrishnavk92@gmail.com>
2025-05-04 22:44:57 +0530

    sunday 4t may 2025

---
+++ learning/python/arithmatical.py
@@ -0,0 +1,26 @@
+a = 50
+b = 20
+
+print(a + b) # Addition
+print(a - b) # Subtraction
+print(a * b) # Multiplication
+print(a / b) # Division
+print(a % b) # Modulus
+print(a ** b) # Exponentiation
+print(a // b) # Floor Division
+print(a & b) # Bitwise AND
+print(a | b) # Bitwise OR
+print(a ^ b) # Bitwise XOR
+print(a << b) # Bitwise Left Shift
+print(a >> b) # Bitwise Right Shift
+print(a == b) # Equality
+print(a != b) # Inequality
+print(a > b) # Greater than
+print(a < b) # Less than
+print(a >= b) # Greater than or equal to
+print(a <= b) # Less than or equal to
+print(a is b) # Identity
+print(a is not b) # Non-identity
+print(a and b) # Logical AND
+print(a or b) # Logical OR
+print(not a) # Logical NOT
