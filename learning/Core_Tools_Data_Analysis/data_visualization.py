8053b58edc0442b6927817e2371bf5efe91a3b0a
vijayakrishna92 <vijaykrishnavk92@gmail.com>
2025-05-09 00:07:37 +0530

    9 may p1

--- learning/Core_Tools_Data_Analysis/data_visualization.py
+++ learning/Core_Tools_Data_Analysis/data_visualization.py
@@ -1,5 +1,10 @@
 #data visualization
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("darkgrid") # Set the style of the seaborn plot
+sns.set_palette("pastel") # Set the color palette of the seaborn plot
+sns.set_context("notebook") # Set the context of the seaborn plot
 
 x = [1, 2, 3, 4, 5]
 y = [2, 3, 5, 7, 11]
@@ -32,3 +37,26 @@ plt.ylabel('Y-axis')
 plt.legend()
 plt.show()  
 plt.savefig('images/plot_4.png')  # Save the scatter plot as a PNG file
+
+sns.scatterplot(x=x, y=y)
+plt.title("Scatter Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+plt.savefig('images/scatterplot.png')  # Save the scatter plot as a PNG file
+
+# Categorical Plots
+sns.barplot(x=x, y=y)
+plt.title("Bar Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+plt.savefig('images/barplot.png')  # Save the line plot as a PNG file
+
+"""
+Categorical Plots - sns.countplot(), sns.boxplot(), sns.violinplot(), sns.stripplot(), sns.swarmplot()
+Distribution Plots - sns.histplot(), sns.kdeplot(), sns.rugplot(), sns.ecdfplot()
+Relationship Plots - sns.scatterplot(), sns.lineplot(), sns.regplot(), sns.jointplot(), sns.pairplot()
+Matrix Plots - sns.heatmap(), sns.clustermap()
+Multiplot Grids (Faceting) - sns.FacetGrid, sns.catplot(), sns.pairplot(), sns.lmplot()
+"""
--- learning/Core_Tools_Data_Analysis/main.py
+++ learning/Core_Tools_Data_Analysis/main.py
@@ -1,2 +1,11 @@
+#plotly – visualization
+
+
+
+
+
+
+
+
 
 
