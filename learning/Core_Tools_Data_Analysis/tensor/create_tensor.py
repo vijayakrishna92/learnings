655198fa69e5b766ff5cae08ea3a60ca02fc6f60
vijayakrishna92 <vijaykrishnavk92@gmail.com>
2025-05-14 18:58:37 +0530

    14 may p4

--- learning/Core_Tools_Data_Analysis/tensor/create_tensor.py
+++ learning/Core_Tools_Data_Analysis/tensor/create_tensor.py
@@ -15,3 +15,18 @@ dim1 = tf.shape(tensors)[0] # Get the first dimension of the tensor
 print(dim1) # Get the first dimension of the tensor
 dim2 = tf.shape(tensors)[1] # Get the second dimension of the tensor
 print(dim2) # Get the second dimension of the tensor
+
+# slicer lets you grab specific sections of your data
+tensors = tf.constant([1,2,3, 4,5,6])
+slicer = tf.slice(tensors , begin=[0], size=[3])
+print(slicer)
+
+tensors = tf.constant([[1,2,3],[4,5,6]])
+slicer = tf.slice(tensors, [0,0], [1,1]) # [0,0]- first row first column, Take 1 row, 1 columns
+print(slicer) # tf.Tensor([[1]], shape=(1, 1), dtype=int32)
+
+# gather = lets you grab specific elements or chunks of your data
+tensors = tf.constant([1,2,3,4,5,6])
+fav = tf.gather(tensors, indices=[1,2,3,4])
+print(fav)
+print(fav.numpy())
